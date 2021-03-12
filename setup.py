53557da2d9456c4223dcbcd63801fef37c5362ed
lyc <lyc@kz.com>
2021-03-12 17:04:57 +0800

    modify version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ twine upload -u [username] -p [password] dist/*
 python setup.py install
 '''
 
-DEFINE_VERSION = '12.94.7'
+DEFINE_VERSION = '12.94.8'
 from setuptools import setup
 import platform
 system = platform.system()
