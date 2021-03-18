70b37b5b18156835ab7b5a6d7391eeda6a36f983
lyc <lyc@kz.com>
2021-03-18 20:34:34 +0800

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -47,15 +47,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+from recommonmark.parser import CommonMarkParser
 source_parsers = {
-  '.md': 'recommonmark.parser.CommonMarkParser',
+    '.md': CommonMarkParser,
 }
-
-source_suffix = ['.md']
+source_suffix = ['.rst', '.md']
