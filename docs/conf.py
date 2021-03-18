ac19385ff1c2ed364df3e0738c2b7276b889eb44
lyc <lyc@kz.com>
2021-03-18 21:05:26 +0800

    modify

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-source_suffix = ['.md']
