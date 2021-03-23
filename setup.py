681787b6ee1ab1a9ff86244dfd8bd36d9aef8f7f
lyc <lyc@kz.com>
2021-03-23 21:12:26 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ twine upload -u [username] -p [password] dist/*
 python setup.py install
 '''
 
-DEFINE_VERSION = '0.0.2'
+DEFINE_VERSION = '0.0.3'
 from setuptools import setup
 import platform
 system = platform.system()
