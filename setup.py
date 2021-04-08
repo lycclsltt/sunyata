8b34944aa1dc01898ec5570ff5c4b0d652632491
lyc <lyc@kz.com>
2021-04-08 21:05:58 +0800

    移除无用代码

--- setup.py
+++ setup.py
@@ -1,24 +1,8 @@
-#!/usr/bin/env python
-'''
-pub.sh
-
-#!/bin/sh
-rm -rf ./agileutil.egg-info
-rm -rf ./build
-rm -rf ./dist
-rm -rf ./*.tar.gz
-python setup.py sdist
-twine upload -u [username] -p [password] dist/*
-python setup.py install
-'''
+#coding=utf-8
 
 DEFINE_VERSION = '0.0.12'
 from setuptools import setup
-import platform
-system = platform.system()
-webpy = 'web.py'
-if platform.python_version()[0:1] == '3':
-    webpy = 'web.py==0.40.dev1'
+
 requireList = [
     'lz4==3.1.3',
     'requests==2.25.1',
@@ -26,9 +10,6 @@ requireList = [
     'DBUtils==1.3',
     'sanic==20.12.3',
 ]
-#if requireList == 'Linux':
-#    requireList.append('japronto')
-#    requireList.append('sanic')
 
 setup(
     name='agileutil',
@@ -45,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='agileutil',
     packages=['agileutil', 'agileutil/rpc', 'agileutil/algorithm'],
